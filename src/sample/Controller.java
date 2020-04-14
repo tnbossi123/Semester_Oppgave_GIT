3db3ea907c569e356562c6996c0baba415ccd567
Ardian <ardian1811@gmail.com>
2020-04-14 21:02:06 +0200

    jeg tester

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import java.awt.event.ActionEvent;
 
 public class Controller {
+
+    @FXML
+    private TextField brukernavnID;
+
+    @FXML
+    private PasswordField passordID;
+
+    @FXML
+    void btnLog(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegister(ActionEvent event) {
+
+    }
+
 }
