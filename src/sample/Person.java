6105167f85690546cd74774a8fd6e0d6299ee4e2
Ardian <ardian1811@gmail.com>
2020-04-14 20:12:46 +0200

    jeg tester

--- src/sample/Person.java
+++ src/sample/Person.java
@@ -5,10 +5,12 @@ public class Person {
 
     String navn;
     String alder;
+    String kjønn;
 
-    public Person(String navn, String alder){
+    public Person(String navn, String alder, String kjønn){
         this.navn = navn;
         this.alder = alder;
+        this.kjønn = kjønn;
     }
 
 }
