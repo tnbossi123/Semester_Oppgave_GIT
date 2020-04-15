d5cb5319d2c852a711e412870fd4794d428bfb6d
Blerton <tnbossi@me.com>
2020-04-15 22:36:16 +0200

    Added Person, Customer and Admin in Person class

--- src/sample/Person.java
+++ src/sample/Person.java
@@ -1,16 +1,88 @@
 package sample;
 
+// Person
 public class Person {
-    //Blerton
+    String name;
+    Dato age;
+    String email;
+    String phoneNumber;
 
-    String navn;
-    String alder;
-    String kjønn;
-
-    public Person(String navn, String alder, String kjønn){
-        this.navn = navn;
+    public Person(String name, Dato age, String email, String phoneNumber){
+        this.name = name;
         this.alder = alder;
-        this.kjønn = kjønn;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Dato getAge() {
+        return age;
+    }
+
+    public void setAge(Dato age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
 }
+
+//Customer
+class Customer extends Person{
+
+    int kundeId;
+
+    public Customer(String name, Dato age, String email, String phoneNumber, int kundeId){
+
+        super(name, age, email, phoneNumber);
+        this.kundeId = kundeId;
+
+    }
+
+    public int getKundeId() {
+        return kundeId;
+    }
+
+    public void setKundeId(int kundeId) {
+        this.kundeId = kundeId;
+    }
+
+}
+
+// Admin user
+class Admin extends Person {
+
+    int adminId;
+
+    public Admin(String name, Dato age, String email, String phoneNumber, int adminId){
+
+        super(name, age, email, phoneNumber);
+        this.adminId = adminId;
+
+    }
+
+}
+
+
+
