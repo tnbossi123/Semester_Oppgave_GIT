a377fc152c1e49d02bab2173c4dacb101e478118
Blerton <tnbossi@me.com>
2020-04-28 01:02:27 +0200

    Added CustomerValidator and CustomerValidatorTest, pluss content

--- src/sample/CustomerValidatorTest.java
+++ src/sample/CustomerValidatorTest.java
@@ -1,7 +1,105 @@
 package src.sample;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-class CustomerValidatorTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static src.sample.CustomerValidator.emailValidator;
+import static src.sample.CustomerValidator.nameValidator;
+import static src.sample.CustomerValidator.numberValidator;
+
+
+class PersonValidatorTest {
+
+    // Valid Names
+    @Test
+    public void testValidNavn(){
+        nameValidator("Per");
+        nameValidator("Billy Bazyy");
+        nameValidator("Ørn Ærlingså");
+
+    }
+
+    // Invalid Names
+    @Test
+    public void testInvalidNavn(){
+
+        assertThrows(IllegalArgumentException.class, () -> nameValidator(""));
+        assertThrows(IllegalArgumentException.class, () -> nameValidator(" "));
+        assertThrows(IllegalArgumentException.class, () -> nameValidator("123"));
+        assertThrows(IllegalArgumentException.class, () -> nameValidator("Blerton123"));
+        assertThrows(IllegalArgumentException.class, () -> nameValidator("123Blerton"));
+        assertThrows(IllegalArgumentException.class, () -> nameValidator("123Blerton123"));
+        assertThrows(IllegalArgumentException.class, () -> nameValidator("!Blerton"));
+        assertThrows(IllegalArgumentException.class, () -> nameValidator("@Blerton"));
+
+    }
+
+
+    // Valid Email
+
+    @Test
+    public void testValidEmail(){
+
+        emailValidator("blerton.selami@oslomet.no");
+        emailValidator("example@example.com");
+        emailValidator("uk@domain.co.uk");
+
+    }
+
+    // Invalid Email
+
+    @Test
+    public void testInvalidEmail(){
+
+        assertThrows(IllegalArgumentException.class, () -> emailValidator(""));
+        assertThrows(IllegalArgumentException.class, () -> emailValidator("blerton.selami"));
+        assertThrows(IllegalArgumentException.class, () -> emailValidator("@oslomet.no"));
+        assertThrows(IllegalArgumentException.class, () -> emailValidator("blerton.selami@invalid"));
+        assertThrows(IllegalArgumentException.class, () -> emailValidator("test@"));
+        assertThrows(IllegalArgumentException.class, () -> emailValidator(";bot@evil.com"));
+        assertThrows(IllegalArgumentException.class, () -> emailValidator(" "));
+
+    }
+
+
+    // Valid Telephone Number
+
+    @Test
+    public void testValidNumber(){
+
+        numberValidator("12233212");
+        numberValidator("+4712233212");
+        numberValidator("+47 12233212");
+        numberValidator("(+47)12233212");
+        numberValidator("(+47) 12233212");
+        numberValidator("(+47) 12 23 32 12");
+        numberValidator("07911 123456");
+        numberValidator("+44 7911 123456");
+        numberValidator("754-3010");
+        numberValidator("(541) 754-3010");
+        numberValidator("+1-541-754-3010");
+        numberValidator("1-541-754-3010");
+        numberValidator("001-541-754-3010");
+
+    }
+
+    // Invalid Telephone Number
+
+    @Test
+    public void testInvalidNumber(){
+
+        assertThrows(IllegalArgumentException.class, () -> CustomerValidator.numberValidator(""));
+        assertThrows(IllegalArgumentException.class, () -> CustomerValidator.numberValidator(" "));
+        assertThrows(IllegalArgumentException.class, () -> CustomerValidator.numberValidator("abc"));
+        assertThrows(IllegalArgumentException.class, () -> CustomerValidator.numberValidator("-231"));
+        assertThrows(IllegalArgumentException.class, () -> CustomerValidator.numberValidator("123-norway"));
+        assertThrows(IllegalArgumentException.class, () -> CustomerValidator.numberValidator("1-541-æøå-3010"));
+        assertThrows(IllegalArgumentException.class, () -> CustomerValidator.numberValidator("1-541-abc-3010"));
+        assertThrows(IllegalArgumentException.class, () -> CustomerValidator.numberValidator("!%&/"));
+        assertThrows(IllegalArgumentException.class, () -> CustomerValidator.numberValidator("123 123     123 12"));
+
+    }
+
 
 }
+
